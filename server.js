ce618d3dce192e9eddcc3bcafe4839f5a91b7a00
Hoa Nguyen Thai <hoa.nguyen-thai@lazada.com>
2017-03-20 17:09:58 +0700

    add page

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(express.static('.'));
-app.use(express.static('dist'));
-
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}!`)
-});
