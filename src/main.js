60b36865b07f03eaee326a5efb051f18b470c3bd
Hoa Nguyen Thai <hoa.nguyen-thai@lazada.com>
2017-03-21 10:34:13 +0700

    fix

--- src/main.js
+++ src/main.js
@@ -1,21 +1,26 @@
 import './main.css';
 
 let _hash = null;
-
+let _intervalDelay = 7000;
 
 function renderPopup(data) {
+
     return `<div class="pending-questions">
         You have total
-        <a class="pending-questions__link" target="_blank" href="${data.url}">${data.total}</a>
+        <a class="pending-questions__link" target="_blank" href="${data.url}" id="total-pending-question">${data.total}</a>
         pending questions!<br/>
         <small>(I am created by Js after AJAX)</small>
     </div>`
 }
 
 function appendPopup(html) {
-    const div = document.createElement('div');
-    div.innerHTML = html;
 
+    var $prev = document.getElementById('pending-questions');
+    $prev && $prev.remove();
+
+    const div = document.createElement('div');
+    div.setAttribute('id', 'pending-questions');
+    div.innerHTML = html;
     document.body.appendChild(div);
 
     setTimeout(() => {
@@ -23,17 +28,36 @@ function appendPopup(html) {
     }, 200);
 }
 
+function getData() {
+    return fetch(`https://demo8280979.mockable.io/pending-questions?hash=${_hash}`)
+        .then((res) => res.json());
+}
+
+function updatePopup({total}) {
+    document.getElementById('total-pending-question').innerHTML = total;
+}
+
 export function addHash(hash) {
     _hash = hash;
+}
+
+export function setIntervalTimeout(interval) {
+    _intervalDelay = interval;
 }
 
 export function init() {
     if (typeof _hash !== 'string') {
         throw new Error('Please addHash([hash])');
     }
-    return fetch(`https://demo8280979.mockable.io/pending-questions?hash=${_hash}`)
-        .then((res) => res.json())
+    getData()
         .then(renderPopup)
         .then(appendPopup)
         .catch((err) => console.warn(err));
+
+    setInterval(() => {
+        getData()
+            .then(renderPopup)
+            .then(appendPopup)
+            .catch((err) => console.warn(err));
+    }, _intervalDelay);
 }
